c1d2f8eb22db7cd49b21b5182c57dfc56b44b0a7
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-11-25 19:30:38 -0500

    Add search and filter functionality to job index page

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,7 +1,12 @@
 <x-layout>
     <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index'), $job->title => '#']" />
 
-    <x-job-card :$job />
+    <x-job-card :$job>
+        <div class="text-slate-500 mb-4 text-sm">
+            {{-- to scape the html we use nl2br the e scape the htlm again --}}
+            {!! nl2br(e($job->description)) !!}
+        </div>
+    </x-job-card>
     <div>
         <x-link-button :href="route('jobs.index')">
             Back to Jobs

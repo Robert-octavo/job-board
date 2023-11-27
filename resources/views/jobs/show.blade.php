725ccd8f64b59d2145080d2f35eadd269da63fbf
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-11-26 19:41:37 -0500

    Fix job show page to display more jobs from the same employer

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -7,6 +7,29 @@
             {!! nl2br(e($job->description)) !!}
         </div>
     </x-job-card>
+
+    <x-card class="mb-4">
+        <h2 class="mb-4 text-lg font-medium">
+            More {{ $job->employer->company_name }} Jobs
+        </h2>
+
+        <div class="text-sm text-slate-500">
+            @foreach ($job->employer->jobs->except($job->id) as $otherJob)
+                <div class="mb-4 flex justify-between">
+                    <div>
+                        <div class="text-slate-700">
+                            <a href="{{ route('jobs.show', $otherJob) }}">
+                                {{ $otherJob->title }}
+                            </a>
+                        </div>
+                        <div class="text-sm">{{ $otherJob->created_at->diffForHumans() }}</div>
+                    </div>
+                    <div class="text-sm">${{ number_format($otherJob->salary) }}</div>
+                </div>
+            @endforeach
+        </div>
+    </x-card>
+
     <div>
         <x-link-button :href="route('jobs.index')">
             Back to Jobs
